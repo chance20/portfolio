d5965c2c650ef8e87d8638a524c3914888b54a0a
Michee Nonga <nongamichee@gmail.com>
2022-06-26 03:08:30 +0100

    resolving conflicts

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ const index = () => {
   return (
     <div className='flex flex-col px-6 pt-1'>
       <h5 className='my-3 font-medium'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat nihil quibusdam minima assumenda, nisi, nostrum voluptates dolor qui consequuntur ipsum veniam debitis molestias, dolores accusamus. Voluptatum aliquam aut dignissimos asperiores.
-      Facilis a enim unde incidunt quae nam sapiente maiores commodi dolorem, in iste cumque expedita, dolores eos ex cum illo eveniet, ipsum fuga dolore quasi! Maiores nihil quas consectetur hic?</h5>
+      Facilis a enim unde incidunt quae nam sapiente maiores commodi dolorem, in iste cumque expedita, dolores eos ex cum illo eveniet, ipsu fuga dolore quasi! Maiores nihil quas consectetur hic?</h5>
       <div className='p-4 mt-5 bg-gray-400 rounded-b-lg dark:bg-dark-100' style={{marginLeft:'-1.5rem', marginRight:'-1.5rem'}}>
         <h6 className='my-3 text-xl font-bold tracking-wide'>Qu &apos; est ce que j &apos; offre</h6>
         <div className='grid gap-4 lg:grid-cols-2'>
