0b3ecb026f173bb4c72fc0ebb2ca43ac23b57f81
Michee Nonga <nongamichee@gmail.com>
2022-06-26 18:40:34 +0100

    fix bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSidePropsContext } from 'next'
+
 import { ServiceCard } from '../components/ServiceCard'
 import {services} from '../data'
 const index = () => {
@@ -23,15 +23,4 @@ const index = () => {
 
 export default index
 
-export const getServerSideProps = async (context:GetServerSidePropsContext) => {
 
-  const res = await fetch("http://localhost:3000/api/services")
-  const data = await res.json()
-
-  console.log("SERVER", services)
-  return {
-    props: {
-      services : data.services
-    },
-  }
-}
