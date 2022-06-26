0c288190c9ddd4c19a3979732e72706d6e40dc49
Michee Nonga <nongamichee@gmail.com>
2022-06-26 01:25:43 +0100

    resolving conflicts

--- type.ts
+++ type.ts
@@ -23,4 +23,4 @@ export interface IProject{
     key_techs:string[]
 }
 
-export type Category = "react" | "mongo"  | "django" | "node" 
+export type Category = "react" | "mongo"  | "django" | "node" | "flutter"
